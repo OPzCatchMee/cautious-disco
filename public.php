b21495a8d93238e845c2bb21a7527773145e4e64
azntegman <jeffin.ng@gmail.com>
2017-04-01 22:51:17 -0500

    Update public.php
    added links to teams competing in event

--- public.php
+++ public.php
@@ -7,10 +7,13 @@ echo '<h1>Our Upcoming Events!</h1><br />';
 
 require ('./mysqli_connect.php');
 
-$q = "SELECT ID,Location_Name, Street, City, State, ZIP, DATE_FORMAT(Date, '%M %d, %Y') AS Date, TIME_FORMAT(Time, '%H:%i')AS Time, Competition_Name 
-FROM MEET
-WHERE Date>=CURRENT_DATE()
-ORDER BY Date DESC";		
+$team = (isset($_GET['team'])) ? $_GET['team'] : 'none';
+if($team=='none')
+{
+	$q = "SELECT ID,Location_Name, Street, City, State, ZIP, DATE_FORMAT(Date, '%M %d, %Y') AS Date, TIME_FORMAT(Time, '%H:%i')AS Time, Competition_Name 
+	FROM MEET
+	WHERE Date>=CURRENT_DATE()
+	ORDER BY Date DESC";
 $r = @mysqli_query ($dbc, $q); // Run the query.
 echo '<table align="center" cellspacing="0" cellpadding="5" width="75%">
 <tr>
@@ -26,10 +29,9 @@ $meetIDS = array();
 $x=0;
 while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         $meetIDS[$x] = $row['ID'];//place id of each meet in an array
-        $x++;//increment the array
 	$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
 		echo '<tr bgcolor="' . $bg . '">
-		<td align="left">' . $row['Location_Name'] . '</td>
+		<td align="left"><a href="/public.php?team=' . $meetIDS[$x] . '">' . $row['Location_Name'] . '</a></td>
 		<td align="left">' . $row['Street'] .' ' . $row['City'] .' , ' . $row['State'] .' ' . $row['ZIP'] .'</td>
                  <!--add a link to another php page to view teams competing according to meet id -->
                 <td align="left">' . $row['Competition_Name'] . '</td>
@@ -38,8 +40,37 @@ while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
 
 	</tr>
 	';
+        $x++;//increment the array
 } // End of WHILE loop.
 echo '</table>';
+}
+else
+{
+	$q = "SELECT a.Competition_Name, b.Team_Name, c.Team_ID, c.Meet_ID 
+	FROM MEET AS a, TEAM AS b, TEAM_COMPETES_AT AS c 
+	WHERE c.MEET_ID=$team && a.id = b.Team_ID and b.Team_ID = c.TEAM_ID 
+	ORDER BY a.Date DESC";
+$r = @mysqli_query ($dbc, $q); // Run the query.
+echo '<center><h2>Teams Competing in this Competition</h2></center>
+<table align="center" cellspacing="0" cellpadding="5" width="75%">
+<tr>
+	<td align="left"><b>Competition Name</b></td>
+	<td align="left"><b>Team Name</b></td>
+</tr>
+';
+$bg = '#eeeeee'; 
+while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+	$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
+		echo '<tr bgcolor="' . $bg . '">
+		<td align="left">' . $row['Competition_Name'] . '</td>
+		<td align="left">' . $row['Team_Name'] .'</td>
+	</tr>
+	';
+} // End of WHILE loop.
+echo '</table>';
+echo'<br /><a href="public.php">Go Back to Upcoming Events</a>';
+}
+
 mysqli_free_result ($r);
 mysqli_close($dbc);
 
