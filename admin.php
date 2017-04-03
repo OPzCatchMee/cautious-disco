ed6cf4936e4629d76897099178d213c65d8224ba
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-03 17:02:47 -0500

    admin

--- admin.php
+++ admin.php
@@ -4,15 +4,7 @@
 $page_title = 'Admin Page';
 include ('includesAdmin/adminHeader.html');
 
-// Print any error messages, if they exist:
-if (isset($errors) && !empty($errors)) {
-	echo '<h1>Error!</h1>
-	<p class="error">The following error(s) occurred:<br />';
-	foreach ($errors as $msg) {
-		echo " - $msg<br />\n";
-	}
-	echo '</p><p>Please try again.</p>';
-}
+
 
 include ('includesAdmin/adminFooter.html');
 ?>
