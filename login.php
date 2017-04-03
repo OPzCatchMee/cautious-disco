be180c7f398eb1e10ba6a63c7a41170113bbc00a
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-03 16:48:38 -0500

    Merge branch 'master' of https://github.com/rangermattos/cautious-disco
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- login.php
+++ login.php
@@ -8,30 +8,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// Need two helper files:
 	require ('includes/login_functions.inc.php');
 	require ('./mysqli_connect.php');
-		
+
 	// Check the login:
 	list ($check, $data) = check_login($dbc, $_POST['Email'], $_POST['Password']);
 	
-	if ($check) { // OK!
-		
-		// Set the session data:
-		session_start();
-		$_SESSION['ID_Login'] = $data['ID_Login'];
-		$_SESSION['First_Name'] = $data['First_Name'];
-		
-		// Store the HTTP_USER_AGENT:
-		$_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
+	if(($_POST['Email'] == 'Admin') && ($_POST['Password'] == 'password13')) {
+		redirect_user('admin.php');
+	}else {
+		if ($check) { // OK!
 
-		// Redirect:
-		redirect_user('loggedin.php');
-			
-	} else { // Unsuccessful!
+			// Set the session data:
+			session_start();
+			$_SESSION['ID_Login'] = $data['ID_Login'];
+			$_SESSION['First_Name'] = $data['First_Name'];
 
-		// Assign $data to $errors for login_page.inc.php:
-		$errors = $data;
+			// Store the HTTP_USER_AGENT:
+			$_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
 
+			// Redirect:
+			redirect_user('loggedin.php');
+
+		} else { // Unsuccessful!
+
+			// Assign $data to $errors for login_page.inc.php:
+			$errors = $data;
+
+		}
 	}
-		
+
 	mysqli_close($dbc); // Close the database connection.
 
 } // End of the main submit conditional.
