2556cc8264976bb2799c5cc20ffd85fe635b4b45
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-03 16:25:57 -0500

    Admin page

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+
+// Include the header:
+$page_title = 'Admin Page';
+include ('includesAdmin/header.html');
+
+// Print any error messages, if they exist:
+if (isset($errors) && !empty($errors)) {
+	echo '<h1>Error!</h1>
+	<p class="error">The following error(s) occurred:<br />';
+	foreach ($errors as $msg) {
+		echo " - $msg<br />\n";
+	}
+	echo '</p><p>Please try again.</p>';
+}
+?>
+
+<?php include ('includesAdmin/footer.html'); ?>
