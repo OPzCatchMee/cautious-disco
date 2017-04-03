411a2b995230d7b040337114a689f3afe2babf4d
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-03 17:06:47 -0500

    Initial commit

--- login.php
+++ login.php
@@ -1,10 +1,8 @@
 <?php # login.php #4
 // This page processes the login form submission.
 // The script now stores the HTTP_USER_AGENT value for added security.
-
 // Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
 	// Need two helper files:
 	require ('includes/login_functions.inc.php');
 	require ('./mysqli_connect.php');
@@ -12,31 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// Check the login:
 	list ($check, $data) = check_login($dbc, $_POST['Email'], $_POST['Password']);
 
-if ($check) { // OK!
+	if ($check) { // OK!
 
-	// Set the session data:
-	session_start();
-	$_SESSION['ID_Login'] = $data['ID_Login'];
-	$_SESSION['First_Name'] = $data['First_Name'];
+		// Set the session data:
+		session_start();
+		$_SESSION['ID_Login'] = $data['ID_Login'];
+		$_SESSION['First_Name'] = $data['First_Name'];
 
-	// Store the HTTP_USER_AGENT:
-	$_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
+		// Store the HTTP_USER_AGENT:
+		$_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
+		// Redirect:
+		redirect_user('loggedin.php');
 
-	// Redirect:
-	redirect_user('loggedin.php');
-
-} else { // Unsuccessful!
-
-	// Assign $data to $errors for login_page.inc.php:
-	$errors = $data;
-
+	} else { // Unsuccessful!
+		// Assign $data to $errors for login_page.inc.php:
+		$errors = $data;
 	}
-}
 
 	mysqli_close($dbc); // Close the database connection.
-
 } // End of the main submit conditional.
-
 // Create the page:
 include ('./login_page.inc.php');
 ?>
