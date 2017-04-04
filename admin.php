36388323e2beee29ca23c09927d41577314eaa18
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-04 14:18:48 -0500

    Location change

--- admin.php
+++ admin.php
@@ -1,9 +1,9 @@
 <?php
 // Include the header:
 $page_title = 'Admin Page';
-include ('./includes/header.html');
+include ('./includesAdmin/adminHeader.html');
 ?>
 
 <h1>Admin</h1>
 
-<?php include ('./includes/footer.html'); ?>
+<?php include ('./includesAdmin/adminFooter.html'); ?>
