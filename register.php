a11be01a26bd51e584a8cb3f44eacdfd00a53414
rangermattos <rangermattos@protonmail.com>
2017-04-10 10:55:16 -0500

    Added emergency contact input to register.php

--- register.php
+++ register.php
@@ -2,12 +2,12 @@
 // This script performs an INSERT query to add a record to the users table.
 
 $page_title = 'Register';
-include ('./includes/header.html');
+include ('includes/header.html');
 
 // Check for form submission:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	require ('./mysqli_connect.php'); // Connect to the db.
+	require ('mysqli_connect.php'); // Connect to the db.
 		
 	$errors = array(); // Initialize an error array.
 	
@@ -97,6 +97,74 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$sex = mysqli_real_escape_string($dbc, trim($_POST['Sex']));
 	}
 	
+	// Check for emergency first name:
+	if (empty($_POST['Emergency_First_Name'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s first name.';
+	} else {
+		$e_fn = mysqli_real_escape_string($dbc, trim($_POST['Emergency_First_Name']));
+	}
+	
+	// Check for emergency last name:
+	if (empty($_POST['Emergency_Last_Name'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s last name.';
+	} else {
+		$e_ln = mysqli_real_escape_string($dbc, trim($_POST['Emergency_Last_Name']));
+	}
+	
+	// Check for emergency email address:
+	if (empty($_POST['Emergency_Email'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s email address.';
+	} else {
+		$e_e = mysqli_real_escape_string($dbc, trim($_POST['Emergency_Email']));
+	}
+	
+	// Check for emergency phone:
+	if (empty($_POST['Emergency_Phone'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s phone number.';
+	} else {
+		$e_phone = mysqli_real_escape_string($dbc, trim($_POST['Emergency_Phone']));
+	}
+	
+	// Check for emergency address:
+	if (empty($_POST['Emergency_Address'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s address.';
+	} else {
+		$e_addr = mysqli_real_escape_string($dbc, trim($_POST['Emergency_Address']));
+	}
+	
+	// Check for emergency address line 2:
+	if (!empty($_POST['Emergency_Address_2'])) {
+		$e_addr .= " " .mysqli_real_escape_string($dbc, trim($_POST['Emergency_Address_2']));
+	}
+	
+	// Check for emergency city:
+	if (empty($_POST['Emergency_City'])) {
+		$errors[] = 'You forgot to enter your emergecy contact\'s city.';
+	} else {
+		$e_city = mysqli_real_escape_string($dbc, trim($_POST['Emergency_City']));
+	}
+	
+	// Check for emergency state:
+	if (empty($_POST['Emergency_State'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s state.';
+	} else {
+		$e_state = mysqli_real_escape_string($dbc, trim($_POST['Emergency_State']));
+	}
+	
+	// Check for emergency zip:
+	if (empty($_POST['Emergency_ZIP'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s ZIP.';
+	} else {
+		$e_zip_code = mysqli_real_escape_string($dbc, trim($_POST['Emergency_ZIP']));
+	}
+	
+	// Check for emergency relationship:
+	if (empty($_POST['Emergency_Relationship'])) {
+		$errors[] = 'You forgot to enter your emergency contact\'s relationship.';
+	} else {
+		$e_relation = mysqli_real_escape_string($dbc, trim($_POST['Emergency_Relationship']));
+	}
+	
 	if (empty($errors)) { // If everything's OK.
 	
 		// Register the user in the database...
@@ -150,10 +218,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						
 		} // End of if ($r) IF.
 		
+		mysqli_free_result($r);
+		
+		// EMERGENCY_CONTACT
+		$q = "INSERT INTO EMERGENCY_CONTACT (Competitor_ID, Emergency_F_Name, Emergency_L_Name, Emergency_Phone, Emergency_Email, Emergency_Street, Emergency_City, Emergency_State, Emergency_ZIP, Emergency_Relationship) VALUES ('$competitor_id', '$e_fn', '$e_ln', '$e_phone', '$e_e', '$e_addr', '$e_city', '$e_state', '$e_zip_code', '$e_relation')";
+		$r = mysqli_query($dbc, $q); // run the query
+		if ($r) {
+			// everything's good
+		} else {
+			echo '<p>Your emergency contact could not be registered. Please contact the administrators.</p>';
+		}
+		
 		mysqli_close($dbc); // Close the database connection.
 
 		// Include the footer and quit the script:
-		include ('./includes/footer.html'); 
+		include ('includes/footer.html'); 
 		exit();
 		
 	} else { // Report the errors.
@@ -163,7 +242,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		foreach ($errors as $msg) { // Print each error.
 			echo " - $msg<br />\n";
 		}
-		echo '</p><p>Please try again.</p><p><br /></p>';
+		echo '</p><p>Please try again.</p><br />';
 		
 	} // End of if (empty($errors)) IF.
 	
@@ -173,6 +252,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 <h1>Register</h1>
 <form action="register.php" method="post">
+	<h2>Personal Info</h2>
 	<div>
 		<label for="First_Name">First Name</label>
 	</div>
@@ -313,11 +393,133 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	</div>
 	<div>
 		<select name="Sex" id="sex" required>
-			<option value="M">M</option>
-			<option value="F">F</option>
+			<option value='M'>M</option>
+			<option value='F'>F</option>
 		</select>
+	</div>
+	
+	<h2>Emergency Contact Info</h2>
+	<div>
+		<label for="Emergency_First_Name">First Name</label>
+	</div>
+	<div>
+		<input type="text" name="Emergency_First_Name" size="15" maxlength="20" value="<?php if (isset($_POST['Emergency_First_Name'])) echo $_POST['Emergency_First_Name']; ?>" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_Last_Name">Last name</label>
+	</div>
+	<div>
+		<input type="text" name="Emergency_Last_Name" size="15" maxlength="40" value="<?php if (isset($_POST['Emergency_Last_Name'])) echo $_POST['Emergency_Last_Name']; ?>" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_Email">Email</label>
+	</div>
+	<div>
+		<input type="text" name="Emergency_Email" size="20" maxlength="60" value="<?php if (isset($_POST['Emergency_Email'])) echo $_POST['Emergency_Email']; ?>" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_Phone" id="emergency_phone_label">Phone Number</label>
+	</div>
+	<div>
+		<input type="tel" maxlength="10" name="Emergency_Phone" id="emergency_phone" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_Address" id="emergency_address_label">Address</label>
+	</div>
+	<div>
+		<input type="text" maxlength="100" name="Emergency_Address" id="emergency_address" required>
+	</div>
+	<div>
+		<label for="Emergency_Address_2" id="emergency_address_2_label"> Address, line 2</label>
+	</div>
+	<div>
+		<input type="text" maxlength="100" name="Emergency_Address_2" id="emergency_address_2">
+	</div>
+	
+	<div>
+		<label for="Emergency_City" id="emergency_city_label">City</label>
+	</div>
+	<div>
+		<input type="text" maxlength="50" name="Emergency_City" id="emergency_city" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_State" id="emergency_state_label">State</label>
+	</div>
+	<div>
+		<select name="Emergency_State" id="emergency_state" required>
+			<option value="AL">Alabama</option>
+			<option value="AK">Alaska</option>
+			<option value="AZ">Arizona</option>
+			<option value="AR">Arkansas</option>
+			<option value="CA">California</option>
+			<option value="CO">Colorado</option>
+			<option value="CT">Connecticut</option>
+			<option value="DE">Delaware</option>
+			<option value="DC">Dist of Columbia</option>
+			<option value="FL">Florida</option>
+			<option value="GA">Georgia</option>
+			<option value="HI">Hawaii</option>
+			<option value="ID">Idaho</option>
+			<option value="IL">Illinois</option>
+			<option value="IN">Indiana</option>
+			<option value="IA">Iowa</option>
+			<option value="KS">Kansas</option>
+			<option value="KY">Kentucky</option>
+			<option value="LA">Louisiana</option>
+			<option value="ME">Maine</option>
+			<option value="MD">Maryland</option>
+			<option value="MA">Massachusetts</option>
+			<option value="MI">Michigan</option>
+			<option value="MN">Minnesota</option>
+			<option value="MS">Mississippi</option>
+			<option value="MO">Missouri</option>
+			<option value="MT">Montana</option>
+			<option value="NE">Nebraska</option>
+			<option value="NV">Nevada</option>
+			<option value="NH">New Hampshire</option>
+			<option value="NJ">New Jersey</option>
+			<option value="NM">New Mexico</option>
+			<option value="NY">New York</option>
+			<option value="NC">North Carolina</option>
+			<option value="ND">North Dakota</option>
+			<option value="OH">Ohio</option>
+			<option value="OK">Oklahoma</option>
+			<option value="OR">Oregon</option>
+			<option value="PA">Pennsylvania</option>
+			<option value="RI">Rhode Island</option>
+			<option value="SC">South Carolina</option>
+			<option value="SD">South Dakota</option>
+			<option value="TN">Tennessee</option>
+			<option value="TX">Texas</option>
+			<option value="UT">Utah</option>
+			<option value="VT">Vermont</option>
+			<option value="VA">Virginia</option>
+			<option value="WA">Washington</option>
+			<option value="WV">West Virginia</option>
+			<option value="WI">Wisconsin</option>
+			<option value="WY">Wyoming</option>
+		</select>
+	</div>
+	
+	<div>
+		<label for="Emergency_ZIP" id="emergency_postal_code_label">ZIP or Postal Code</label>
+	</div>
+	<div>
+		<input type="text" maxlength="16" name="Emergency_ZIP" id="emergency_postal_code" required>
+	</div>
+	
+	<div>
+		<label for="Emergency_Relationship" id="emergency_relationship">Relationship</label>
+	</div>
+	<div>
+		<input type="text" maxlength="16" name="Emergency_Relationship" id="emergency_relationship" required>
 	</div>
 	
 	<div><input type="submit" name="submit" value="Register"></div>
 </form>
-<?php include ('./includes/footer.html'); ?>
+<?php include ('includes/footer.html'); ?>
