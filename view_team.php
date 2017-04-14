ad15193bcb4eb60725adc4be149c2bd92b779f9a
azntegman <jeffin.ng@gmail.com>
2017-04-13 23:11:15 -0500

    added back the number of members in a team
    Added another query to count number of members in a team

--- view_team.php
+++ view_team.php
@@ -11,17 +11,28 @@ if(isset($_GET['id']))
 {
 	$team_id = $_GET['id'];
 	
+	//for team info
 	$q = "SELECT Team_Name, Street, City, State, ZIP
 	FROM TEAM
 	WHERE Team_ID=$team_id";
+	
+	//to count the number of team members
+	$q2 = "SELECT a.Team_ID, b.Team_ID
+	FROM TEAM as a, COMPETITOR as b
+	WHERE a.Team_ID=$team_id & b.Team_ID=$team_id";
+	
 	$r = @mysqli_query ($dbc, $q); // Run the query.
 	
 	if (mysqli_affected_rows($dbc) == 1) // should only find one result
 	{
 		$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
 		echo '<p>Team Name: '.$row['Team_Name'].'</p>
-		<p>Address: '.$row['Street'].' '.$row['City'].' '.$row['State'].' '.$row['ZIP'].'</p>
-		<!--<p>Number of Members: '.$row['Num_Members'].'</p>will add correct query when I get it-->'; 
+		<p>Address: '.$row['Street'].' '.$row['City'].' '.$row['State'].' '.$row['ZIP'].'</p>';
+		//free the result to start second query
+        	mysqli_free_result ($r);
+        	$r = @mysqli_query ($dbc, $q2);
+        	$rowcount = mysqli_num_rows($r);//counts number of rows which is number of members of a team
+		echo '<p>Number of Members: '.$rowcount.'</p>';
 	}
 	else
 	{
