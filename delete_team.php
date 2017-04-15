e32e39603dd8329965b1d2156d3be9f3345e67c3
azntegman <jeffin.ng@gmail.com>
2017-04-15 00:34:31 -0500

    trying different query
    Post doesnt work well in my local environment. :( Have to test a little here

--- delete_team.php
+++ delete_team.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_POST['sure'] == 'Yes') { // Delete the record.
 
         // Make the query:
-        $q = "UPDATE TEAM SET Deleted = 1 WHERE Team_ID=$id";
+        $q = "UPDATE TEAM SET Deleted = 1 WHERE Team_ID=$id LIMIT 1";
 	    //UPDATE TEAM SET Deleted = 1 WHERE Team_ID=$id LIMIT 1
         $r = @mysqli_query ($dbc, $q);
         if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
