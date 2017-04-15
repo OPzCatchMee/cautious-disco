c794796f5d12abc23ace0417e791c0ba554c4915
rangermattos <rangermattos@protonmail.com>
2017-04-15 15:14:36 -0500

    Updated create_event.php to use dropdowns, use user entering as judge

--- create_event.php
+++ create_event.php
@@ -3,10 +3,10 @@
 
 $page_title = 'Create Event';
 include ('includes/header.html');
+	require('mysqli_connect.php');
 
 // Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	require('mysqli_connect.php');
 
 	$errors = array();
 
@@ -23,13 +23,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	} else {
 		$meet = mysqli_real_escape_string($dbc, trim($_POST['Meet_ID']));
 	}
-
-	// Check for a judge id:
-	if (!isset($_POST['Judge_ID'])) {
-		$errors[] = 'You forgot to enter judge id.';
-	} else {
-		$judge = mysqli_real_escape_string($dbc, trim($_POST['Judge_ID']));
+	
+	// Check if that competitor is signed up for that meet
+	$q = "SELECT Fee_Paid
+		FROM COMPETITOR_COMPETES_AT
+		WHERE Meet_ID=$meet AND Competitor_ID=$competitor";
+	$r = @mysqli_query($dbc, $q);
+	if (mysqli_num_rows($r) == 0)
+	{
+		$errors[] = "That competitor is not registered for this meet.";
 	}
+
+	$judge = $_SESSION['Staff_ID']; // judge is the person entering the form
 
 	// Check for a Event type:
 	if (!isset($_POST['Event_Type'])) {
@@ -38,28 +43,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$event = mysqli_real_escape_string($dbc, trim($_POST['Event_Type']));
 	}
 
-  // Check for a execution score:
-  if (!isset($_POST['Exec_Score'])) {
-    $errors[] = 'You forgot to enter the event type.';
-  } else {
-    $exeScore = mysqli_real_escape_string($dbc, trim($_POST['Exec_Score']));
-  }
+	// Check for a execution score:
+	if (!isset($_POST['Exec_Score'])) {
+		$errors[] = 'You forgot to enter the event type.';
+	} else {
+		$exeScore = mysqli_real_escape_string($dbc, trim($_POST['Exec_Score']));
+	}
 
-  // Check for a Difficulty Score:
-  if (!isset($_POST['Difficulty_Score'])) {
-    $errors[] = 'You forgot to enter the difficulty score.';
-  } else {
-    $difScore = mysqli_real_escape_string($dbc, trim($_POST['Difficulty_Score']));
-  }
+	// Check for a Difficulty Score:
+	if (!isset($_POST['Difficulty_Score'])) {
+		$errors[] = 'You forgot to enter the difficulty score.';
+	} else {
+		$difScore = mysqli_real_escape_string($dbc, trim($_POST['Difficulty_Score']));
+	}
 
-  // Check for a Date:
+	// Check for a Date:
 	if (!isset($_POST['Date'])) {
 		$errors[] = 'You forgot to enter date.';
 	} else {
 		$date = mysqli_real_escape_string($dbc, trim(\DateTime::createFromFormat('m-d-Y',$_POST['Date'])->format('Y-m-d'))); // assume came in the form  MM-DD-YYYY
 	}
 
-  // Check for a Time:
+	// Check for a Time:
 	if (!isset($_POST['Time'])) {
 		$errors[] = 'You forgot to enter time.';
 	} else {
@@ -69,7 +74,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (empty($errors)) { // If everything's OK.
 
 		// Register the event in the database...
-		$q = "INSERT INTO EVENT (Competitor_ID, Meet_ID, Judge_ID, Event_Type, Exec_Score, Difficulty_Score, Date, Time) VALUES ( '$competitor', '$meet', '$judge', '$event', '$exeScore', '$difScore','$date', '$time')";
+		$q = "INSERT INTO EVENT (Competitor_ID, Meet_ID, Judge_ID, Event_Type, Exec_Score, Difficulty_Score, Date, Time) VALUES ( '$competitor', '$meet', '$judge', '$event', '$exeScore', '$difScore', '$date', '$time')";
 		$r = @mysqli_query($dbc, $q);
 
 		if (!$r)
@@ -79,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<p class="error">The event could not be registered due to a system error. We apologize for any inconvenience.</p>';
 
 			// Debugging message:
-			echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
+			echo '<p>' . mysqli_error($dbc) . '</p><p>Query: ' . $q . '</p>';
 		} else { // If it ran OK.
 
 			// Print a message:
@@ -96,15 +101,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	} else { // Report the errors.
 
 		echo '<h1>Error!</h1>
-		<p class="error">The following error(s) occurred:<br />';
+		<p class="error">The following error(s) occurred:</p>';
 		foreach ($errors as $msg) { // Print each error.
-			echo " - $msg<br />\n";
+			echo '<p class="error"> - ' . $msg . '</p>';
 		}
-		echo '</p><p>Please try again.</p><p><br /></p>';
+		echo '<p>Please try again.</p>';
 
 	} // End of if (empty($errors)) IF.
-
-	mysqli_close($dbc); // Close the database connection.
 
 } // End of the main Submit conditional.
 ?>
@@ -118,17 +121,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	</div>
 	<div>
 		<select name="Competitor_ID" id="competitor" required>
-			<option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-			<option value="9">9</option>
-      <option value="10">10</option>
+		<?php
+			$q = "SELECT ID
+				FROM COMPETITOR
+				WHERE Deleted=0";
+			$r = @mysqli_query ($dbc, $q); // Run the query.
+			
+			while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+				echo '
+				<option value="' . $row[0] . '">' . $row[0] . '</option>';
+			}
+			
+			mysqli_free_result ($r);
+		?>
+		
 		</select>
 	</div>
 
@@ -138,37 +144,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	</div>
 	<div>
 		<select name="Meet_ID" id="meet" required>
-			<option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-		</select>
-	</div>
-
-	<!-- Judge ID field -->
-	<div>
-		<label for="Judge_ID" id="judge_label">Judge ID</label>
-	</div>
-	<div>
-		<select name="Judge_ID" id="judge" required>
-			<option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-			<option value="9">9</option>
-      <option value="10">10</option>
+		<?php
+			$q = "SELECT ID
+				FROM MEET
+				WHERE Deleted=0";
+			$r = @mysqli_query ($dbc, $q); // Run the query.
+			
+			while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+				echo '
+				<option value="' . $row[0] . '">' . $row[0] . '</option>';
+			}
+			
+			mysqli_free_result ($r);
+		?>
+		
 		</select>
 	</div>
 
@@ -182,28 +171,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<option value="balance beam">Balance Beam</option>
 			<option value="vault">Vault</option>
 			<option value="uneven bars">Uneven Bars</option>
-      <option value="pommel horse">Pommel Horse</option>
-      <option value="still rings">still rings</option>
-      <option value="parallel bars">Parallel Bars</option>
-      <option value="horizontal bars">Horizontal Bars</option>
+			<option value="pommel horse">Pommel Horse</option>
+			<option value="still rings">Still rings</option>
+			<option value="parallel bars">Parallel Bars</option>
+			<option value="horizontal bars">Horizontal Bars</option>
 		</select>
 	</div>
 
-  <!-- Execution Score field -->
+	<!-- Execution Score field -->
 	<div>
-		<label for="Exec_Score">Execution Score</label>
+		<label for="Exec_Score" id="exec_score_label">Execution Score</label>
 	</div>
 	<div>
-		<input type="text" maxlength="10" name="Exec_Score" value="<?php if (isset($_POST['Exec_Score'])) echo $_POST['Exec_Score']; ?>" required>
+		<select name="Exec_Score" id="exec_score" required>
+			<?php
+			for ($num = 1; $num <= 10; $num++)
+			{
+				echo '<option value=\"' . $num . '\">' . $num . '</option>';
+			}
+			?>
+			
+		</select>
 	</div>
 
-  <!-- Difficulty Score field -->
-  <div>
-    <label for="Difficulty_Score">Difficulty Score</label>
-  </div>
-  <div>
-    <input type="text" maxlength="10" name="Difficulty_Score" value="<?php if (isset($_POST['Difficulty_Score'])) echo $_POST['Difficulty_Score']; ?>" required>
-  </div>
+	<!-- Difficulty Score field -->
+	<div>
+		<label for="Difficulty_Score" id="difficulty_score_label">Difficulty Score</label>
+	</div>
+	<div>
+		<select name="Difficulty_Score" id="difficulty_score" required>
+			<?php
+			for ($num = 1; $num <= 10; $num++)
+			{
+				echo '<option value=\"' . $num . '\">' . $num . '</option>';
+			}
+			?>
+			
+		</select>
+	</div>
 
 	<!-- Date field -->
 	<div>
@@ -221,6 +226,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		<input type="text" maxlength="5" name="Time" value="<?php if (isset($_POST['Time'])) echo $_POST['Time']; ?>" required>
 	</div>
 
-	<div><input type="submit" name="submit" value="Register"></div>
+	<div><input type="submit" name="submit" value="Sumbit"></div>
 </form>
 <?php include ('includes/footer.html'); ?>
