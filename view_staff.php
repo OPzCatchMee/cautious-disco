ffe27f0da005e4f37ce3d6f55d42a86a057ea1c1
azntegman <jeffin.ng@gmail.com>
2017-04-15 19:54:12 -0500

    view Staff added.  needs to be tested
    Views staff info and has a link to edit staff to edit their own info.  Needs to be tested

---
+++ view_staff.php
@@ -0,0 +1,33 @@
+<?php
+//This will be how staff that are logged in view and edit their info
+
+$page_title = 'Edit Information';
+include ('includes/header.html');
+echo '<h1>Edit Personal Information</h1>';
+
+
+if ((isset($_SESSION['Staff_ID'])))//if logged in as Staff
+{
+    require ('mysqli_connect.php');
+
+        $staff_ID = $_SESSION['Staff_ID'];
+        $q = "SELECT a.Staff, a.User, b.ID_Login, b.Email, b.First_Name, b.Last_Name, DATE_FORMAT(b.Date_Of_Birth, '%M %d, %Y') AS DOB, 
+b.Phone, b.Street, b.City, b.State, b.Zip
+              FROM STAFF_ID as a, LOGIN as b
+              WHERE a.Staff=$staff_ID && a.User=b.ID_Login && b.Deleted=0";
+
+        $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+        echo '<p>Email: '.$row['Email'].'</p>
+              <p>First Name: '.$row['First_Name'].'</p>
+              <p>Last Name: '.$row['Last_Name'].'</p>
+              <p>DOB: '.$row['DOB'].'</p>
+              <p>Phone: '.$row['Phone'].'</p>
+              <p>Address: '.$row['Street'].' '.$row['City'].' '.$row['State'].' '.$row['ZIP'].'</p>';
+        //free the result to start second query
+        mysqli_free_result ($r);
+        echo'<a href="edit_staff.php"> Edit info</a>';
+}
+else
+{
+    echo 'You are not logged in as a staff member or ID is not valid';
+}
