e77f0e71bfa34e7172d8c95aa08861e7b362efe7
OPzCatchMee <cmnguyen999@gmail.com>
2017-04-15 22:56:28 -0500

    Done copy and paste and change php extension

--- newfile/admin-competitions.php
+++ newfile/admin-competitions.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
   <meta name="generator" content="2017.0.2.363"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  
+
   <script type="text/javascript">
    // Update the 'nojs'/'js' class on the html node
 document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
@@ -13,7 +13,7 @@ document.documentElement.className = document.documentElement.className.replace(
 // Check that all required assets are uploaded and up-to-date
 if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["museutils.js", "museconfig.js", "jquery.musemenu.js", "jquery.watch.js", "jquery.museresponsive.js", "require.js", "admin-competitions.css"], "outOfDate":[]};
 </script>
-  
+
   <title>Admin Competitions</title>
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=443350757"/>
@@ -76,7 +76,166 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
      </div>
      <img class="colelem temp_no_img_src" id="u36964-4" alt="Competitions" data-orig-src="images/u36964-4.png?crc=4268490803" data-image-width="630" src="images/blank.gif?crc=4208392903"/><!-- rasterized frame -->
      <div class="colelem shared_content" id="u37058" data-content-guid="u37058_content"><!-- custom html -->
-      <? php><?>
+      <? php
+
+      $page_title = 'View the Competitor';
+      include ('includesAdmin/adminHeader.html');
+      echo '<h1><center>Competitor</center></h1>';
+
+      require ('mysqli_connect.php');
+
+      // Number of records to show per page:
+      $display = 10;
+
+      // Determine how many pages there are...
+      if (isset($_GET['p']) && is_numeric($_GET['p'])) { // Already been determined.
+       $pages = $_GET['p'];
+       } else { // Need to determine.
+      	// Count the number of records:
+        $q = "SELECT COUNT(ID) FROM COMPETITOR";
+        $r = @mysqli_query ($dbc, $q);
+        $row = @mysqli_fetch_array ($r, MYSQLI_NUM);
+        $records = $row[0];
+        // Calculate the number of pages...
+        if ($records > $display) { // More than 1 page.
+          $pages = ceil ($records/$display);
+        } else {
+          $pages = 1;
+        }
+      } // End of p IF.
+
+      // Determine where in the database to start returning results...
+      if (isset($_GET['s']) && is_numeric($_GET['s'])) {
+        $start = $_GET['s'];
+      } else {
+        $start = 0;
+      }
+
+      // Determine the sort...
+      // Default is by first name.
+      $sort = (isset($_GET['sort'])) ? $_GET['sort'] : 'fn';
+
+      // Determine the sorting order:
+      switch ($sort) {
+        case 'fn':
+          $order_by = 'First_Name ASC';
+          break;
+        case 'ln':
+          $order_by = 'Last_Name ASC';
+          break;
+        case 'birth':
+          $order_by = 'Date_Of_Birth ASC';
+          break;
+        case 'street':
+          $order_by = 'Street ASC';
+          break;
+        case 'city':
+          $order_by = 'City ASC';
+          break;
+        case 'state':
+          $order_by = 'State ASC';
+          break;
+        case 'zip':
+          $order_by = 'ZIP ASC';
+          break;
+        case 'phone':
+          $order_by = 'Phone ASC';
+          break;
+        case 'email':
+          $order_by = 'Email ASC';
+          break;
+        case 'lvl':
+          $order_by = 'Level ASC';
+          break;
+        case 'sex':
+          $order_by = 'Sex ASC';
+          break;
+        case 'team_id':
+          $order_by = 'Team_ID ASC';
+          break;
+        default:
+          $order_by = 'First_Name ASC';
+          $sort = 'fn';
+          break;
+      }
+
+      // Define the query:
+      $q = "SELECT ID, First_Name, Last_Name, Email, Date_Of_Birth, Street, City, State, ZIP, Phone, Level, Sex, Team_ID
+        FROM (LOGIN INNER JOIN COMPETITOR_ID ON LOGIN.ID_Login=COMPETITOR_ID.User INNER JOIN COMPETITOR ON COMPETITOR_ID.Competitor=COMPETITOR.ID)
+        WHERE LOGIN.Deleted=0
+        ORDER BY $order_by
+        LIMIT $start, $display";
+      $r = @mysqli_query ($dbc, $q); // Run the query.
+
+      // for debugging
+      echo '<p>Number of results: '.mysqli_affected_rows($dbc).'</p>';
+
+      // Table header:
+      echo '<table>
+      <thead>
+        <th><a href="admin_competitor.php?sort=fn">First Name</a></th>
+        <th><a href="admin_competitor.php?sort=ln">Last Name</a></th>
+        <th><a href="admin_competitor.php?sort=birth">Date of Birth</a></th>
+        <th><a href="admin_competitor.php?sort=street">Street</a></th>
+        <th><a href="admin_competitor.php?sort=city">City</a></th>
+        <th><a href="admin_competitor.php?sort=state">State</a></th>
+        <th><a href="admin_competitor.php?sort=zip">ZIP</a></th>
+        <th><a href="admin_competitor.php?sort=phone">Phone</a></th>
+        <th><a href="admin_competitor.php?sort=email">Email</a></th>
+        <th><a href="admin_competitor.php?sort=lvl">Level</a></th>
+        <th><a href="admin_competitor.php?sort=sex">Sex</a></th>
+        <th><a href="admin_competitor.php?sort=team_id">Team ID</a></th>
+      </thead>
+      ';
+
+      // Fetch and print all the records....
+      while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+        echo '<tr>
+          <td>' . $row['First_Name'] . '</td>
+          <td>' . $row['Last_Name'] . '</td>
+          <td>' . $row['Date_Of_Birth'] . '</td>
+          <td>' . $row['Street'] . '</td>
+          <td>' . $row['City'] . '</td>
+          <td>' . $row['State'] . '</td>
+          <td>' . $row['ZIP'] . '</td>
+          <td>' . $row['Phone'] . '</td>
+          <td>' . $row['Email'] . '</td>
+          <td>' . $row['Level'] . '</td>
+          <td>' . $row['Sex'] . '</td>
+          <td>' . $row['Team_ID'] . '</td>
+          <td><a href="delete_competitor.php?id=' . $row['ID'] . '">Delete</a></td>
+        </tr>
+        ';
+      } // End of WHILE loop.
+
+      echo '</table>';
+      mysqli_free_result ($r);
+      mysqli_close($dbc);
+
+      // Make the links to other pages, if necessary.
+      if ($pages > 1) {
+        echo '<br /><p>';
+        $current_page = ($start/$display) + 1;
+        // If it's not the first page, make a Previous button:
+        if ($current_page != 1) {
+          echo '<a href="admin_competitor.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a> ';
+        }
+        // Make all the numbered pages:
+        for ($i = 1; $i <= $pages; $i++) {
+          if ($i != $current_page) {
+            echo '<a href="admin_competitor.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a> ';
+          } else {
+            echo $i . ' ';
+          }
+        } // End of FOR loop.
+        // If it's not the last page, make a Next button:
+        if ($current_page != $pages) {
+          echo '<a href="admin_competitor.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">Next</a>';
+        }
+      echo '</p>'; // Close the paragraph.
+      } // End of links section.
+
+      ?>
      </div>
     </div>
     <div class="verticalspacer shared_content" data-offset-top="539" data-content-above-spacer="539" data-content-below-spacer="49" data-content-guid="page_3_content"></div>
