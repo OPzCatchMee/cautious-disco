82007f2f6d9a8c1ba341b617ac27f597b43df90e
azntegman <jeffin.ng@gmail.com>
2017-04-24 16:26:16 -0500

    added table width to fix table view

--- newfile/view-staff.php
+++ newfile/view-staff.php
@@ -5,7 +5,11 @@
 <!DOCTYPE html>
 <html class="nojs html css_verticalspacer" lang="en-US">
  <head>
-
+	 <style>
+		 table{
+			 width:100%;
+		 }
+	 </style>
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
   <meta name="generator" content="2017.0.2.363"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
